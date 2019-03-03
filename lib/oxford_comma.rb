f96f8b85cf628560d15f7543dc125c142a297b8d
Danny Wakeling <wakeling101@hotmail.com>
2019-03-03 15:35:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,6 @@ def oxford_comma(array)
   if array.length == 1
     sentence << array.join
   elsif array.length == 2
-    sentence << array[0] + "and" + array[1]
+    sentence << array[0] + " and " + array[1]
   return sentence
 end
